5688eb03be8bd5020685a0d983e61e0ac753a8eb
jawang35 <jawang35@gmail.com>
2018-08-04 16:58:03 -0700

    Make LispVal instance of Show

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,7 +12,18 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
-             deriving (Show)
+
+instance Show LispVal where
+    show (String contents) = "\"" ++ contents ++ "\""
+    show (Atom name) = name
+    show (Number contents) = show contents
+    show (Bool True) = "#t"
+    show (Bool False) = "#f"
+    show (List contents) = "(" ++ unwordsList contents ++ ")"
+    show (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ show tail ++ ")"
+
+unwordsList :: (Show a) => [a] -> String
+unwordsList = unwords . map show
 
 symbol :: Parser Char
 symbol = oneOf "!$%&!*+-/:<=?>@^_~#"
